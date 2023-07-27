3a8b7f089d70219336107fabc8a4506b3e67c8c4
arsen.toksyn <arsen.toksynarsen.toksyn@gmail.com>
2023-07-27 21:24:51 +0600

    This is my firts repo ever

---
+++ python.py
@@ -0,0 +1,25 @@
+f = open("myfile.txt", "w")
+f.write("This is my first file!")
+f.close()
+
+f = open("myfile.txt")
+print(f.read())
+f.close()
+
+f = open("myfile.txt", "a")
+f.write(" And second ...")
+f.close()
+f = open("myfile.txt")
+print(f.read())
+f.close() 
+import os
+
+file_name = "myfile.txt"
+
+# Get the current working directory
+current_dir = os.getcwd()
+
+# Combine the current working directory with the file name to get the full file path
+file_path = os.path.join(current_dir, file_name)
+print("Dir path:", current_dir)
+print("File Path:", file_path)
